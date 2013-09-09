57624af012eee84b3b410ac01f79d712b6cc709a
rwl <r.w.lincoln@gmail.com>
2013-09-09 22:41:37 +0200

    Correcting discovery scheme.

--- discovery.go
+++ discovery.go
@@ -14,8 +14,8 @@ import (
 // The endpoint host we're using to proxy discovery and static requests.
 // Using separate constants to make it easier to change the discovery service.
 var (
-	_DISCOVERY_PROXY_HOST      = "webapis-discovery.appspot.com"
-	_STATIC_PROXY_HOST         = "webapis-discovery.appspot.com"
+	_DISCOVERY_PROXY_HOST      = "https://webapis-discovery.appspot.com"
+	_STATIC_PROXY_HOST         = "https://webapis-discovery.appspot.com"
 	_DISCOVERY_API_PATH_PREFIX = "/_ah/api/discovery/v1/"
 )
 
@@ -45,7 +45,7 @@ func /*(dp *DiscoveryApiProxy)*/ dispatch_discovery_request(path, body string) (
 	full_path := _DISCOVERY_API_PATH_PREFIX + path
 	client := &http.Client{}
 
-	req, err := http.NewRequest("POST", "https://"+_DISCOVERY_PROXY_HOST+full_path, nil)
+	req, err := http.NewRequest("POST", _DISCOVERY_PROXY_HOST+full_path, nil)
 	if err != nil {
 		return "", err
 	}
@@ -123,7 +123,7 @@ func /*(dp *DiscoveryApiProxy)*/ generate_discovery_directory(api_configs []stri
 // proxy host.
 // response_body: A string containing the response body.
 func /*(dp *DiscoveryApiProxy)*/ get_static_file(path string) (*http.Response, string, error) {
-	resp, err := http.Get("https://"+_STATIC_PROXY_HOST + path)
+	resp, err := http.Get(_STATIC_PROXY_HOST + path)
 	if err != nil {
 		return nil, "", err
 	}
